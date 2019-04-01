03316ee5fd11467d3d9f02e5501ab3a9e8d1a2b9
shahramsobhani <44172862+shahramsobhani@users.noreply.github.com>
2019-04-01 13:32:31 -0700

    Create site.pp

---
+++ manifests/site.pp
@@ -0,0 +1,19 @@
+node default {
+	file { '/root/README' :
+		ensure => file,
+		content => "Welcome to ${fqdn}, this is a default server, uptime about ${system_uptime}\n",
+	}
+}
+
+node /^master/ {
+	include role::masterserver
+}
+
+node "puppetmaster.us.lsst.org"{
+	include role::masterserver
+}
+
+node /^web/ {
+	include role::app_server
+}
+
