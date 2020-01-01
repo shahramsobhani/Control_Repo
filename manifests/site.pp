5eb06e425091d5b21fbc89482cc77f3d84bbac12
shahramsobhani <44172862+shahramsobhani@users.noreply.github.com>
2020-01-01 10:57:41 -0700

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,11 +1,13 @@
 # This is the very default configuration, in case no match found in ENC
 node default {
+	file {' /root/README';
+	ensure => file,
+	content => 'this is a readme',
+	}
 #	include role::default
 }
-
 # There is one situation, when deploying a new puppet master, in where the ENC doesn't exists in the puppet environment
 # that's why, puppet-master is the only definition here.
-node /puppet-master/ {
-	include role::it::puppet_master
-}
-
+#node /puppet-master/ {
+#	include role::it::puppet_master
+#}
