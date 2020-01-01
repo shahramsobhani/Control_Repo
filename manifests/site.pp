2afd64de1780bf7ac77aed2b5ca91d3a11876c03
shahramsobhani <44172862+shahramsobhani@users.noreply.github.com>
2020-01-01 13:29:55 -0700

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -5,6 +5,9 @@ node default {
 	content => 'this is a readme',
 	owner   => 'root',
 	}
+node 'master.puppet.vm' {
+  include role::masterserver
+}  
 #	include role::default
 # There is one situation, when deploying a new puppet master, in where the ENC doesn't exists in the puppet environment
 # that's why, puppet-master is the only definition here.
