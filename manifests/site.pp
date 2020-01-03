7579452155e42b098f6367ac10f7f54711420273
shahramsobhani <44172862+shahramsobhani@users.noreply.github.com>
2020-01-03 15:37:36 -0700

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,12 +1,12 @@
 node default {
   }
-}
 node 'master.puppet.vm' {
   include role::masterserver
     file { '/root/README':
       ensure => file,
       content => "welcome to ${fqdn}\n,
       owner   => 'root',
+      }
 }
 node /^web/ { 
   include role::app_server
