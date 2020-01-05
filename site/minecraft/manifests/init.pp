793b125e63bde7673bc5109baae4bbb0f1108b5e
shahramsobhani <44172862+shahramsobhani@users.noreply.github.com>
2020-01-04 21:02:19 -0700

    Update init.pp

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -1,23 +1,23 @@
 class minecraft {
-  file {‘/opt/minecraft’:
+  file {'/opt/minecraft':
     ensure => directory,
   }
-  file {‘/opt/minecraft/server.jar’:
+  file {'/opt/minecraft/server.jar':
     ensure => file,
     source      => 'https://launcher.mojang.com/v1/objects/4d1826eebac84847c71a77f9349cc22afd0cf0a1/server.jar',
   }
-  package {‘java’:
+  package {'java':
     ensure => present,
   }
-  file {‘/opt/minecraft/eula.txt’:
+  file {'/opt/minecraft/eula.txt':
     ensure => file,
     content => ‘eula=true’
   }
-  file {‘/etc/systemd/system/minecraft.service’:
+  file {'/etc/systemd/system/minecraft.service':
     ensure => file,
-    source => ‘puppet:///modules/minecraft/minecraft.service’,
+    source => 'puppet:///modules/minecraft/minecraft.service',
   }
-  service { ‘minecraft’:
+  service { 'minecraft':
     ensure => running,
     enable => true,
   }
